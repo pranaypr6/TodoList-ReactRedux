cd8f5465b9e5290fd55fe06f710b8407776b21a2
unknown <pranayburra66@gmail.com>
2020-06-25 17:16:14 +0530

    Updates

---
+++ precache-manifest.7d4456e0dd744b4f35dfe1f661d2b418.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518fdc612cefcdfe442b5eb8d3ae22c7",
+    "url": "/TodoList-ReactRedux/index.html"
+  },
+  {
+    "revision": "3f6e77b1041e4ba51a33",
+    "url": "/TodoList-ReactRedux/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "398b74d0558fe51e5277",
+    "url": "/TodoList-ReactRedux/static/css/main.38d3d503.chunk.css"
+  },
+  {
+    "revision": "3f6e77b1041e4ba51a33",
+    "url": "/TodoList-ReactRedux/static/js/2.1d24b010.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/TodoList-ReactRedux/static/js/2.1d24b010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "398b74d0558fe51e5277",
+    "url": "/TodoList-ReactRedux/static/js/main.9e18516b.chunk.js"
+  },
+  {
+    "revision": "e0c9bb7a7e8a7daad111",
+    "url": "/TodoList-ReactRedux/static/js/runtime-main.bc0551b0.js"
+  }
+]);
